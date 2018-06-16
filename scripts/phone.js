839b3b05024f9de082477623dc868f2178c5abf5
ElisaCalamita <33039355+ElisaCalamita@users.noreply.github.com>
2018-06-16 21:58:03 +0200

    responsive

--- scripts/phone.js
+++
@@ -1,7 +0,0 @@
-</script>
-<script type="text/javascript">// <![CDATA[ 
-    var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLower Case())); 
-    if (mobile) { 
-        document.location = "http://www.hetschetsboek.nl"; 
-    } 
-// ]]></script> 
